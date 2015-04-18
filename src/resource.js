1122bf6d62c16194df9d0c917ea79905ce4f851f
Andrea Grandi <muffo87@gmail.com>
2015-04-18 11:49:12 +0100

    Basic set of resources for the main menu

--- src/resource.js
+++ src/resource.js
@@ -1,8 +1,18 @@
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
-    CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png"
+    MenuBackground_png : "res/SpaceShooterRedux/Backgrounds/blue_480x320.png",
+        
+    GameBackground_png : "res/SpaceShooterRedux/Backgrounds/purple_960x320.png",
+        
+    // UI
+    ButtonBlue_png : "res/SpaceShooterRedux/PNG/UI/buttonBlue.png",
+    ButtonGreen_png : "res/SpaceShooterRedux/PNG/UI/buttonGreen.png",
+    
+    Kenvector_ttf : "res/SpaceShooterRedux/Bonus/kenvector_future.ttf",
+    
+    
 };
+
+// {fontName:"Kenvector", src:[{src:"res/SpaceShooterRedux/Bonus/kenvector_future.ttf", type:"truetype"}]}
 
 var g_resources = [];
 for (var i in res) {
